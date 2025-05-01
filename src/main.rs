d20b48ef66d5616ef594dc86404301bffe1ca2fb
Rafał Głowacz <rafalglowacz@gmail.com>
2025-05-01 10:31:04 +0200

    Get dir arg with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let args = clap::Command::new("wallpaper-maker")
+        .arg(clap::Arg::new("directory").help("Directory containing images").required(true))
+        .get_matches();
+
+    let path = args.get_one::<String>("directory").expect("Directory is required");
+
+    let path = path.trim_end_matches('/');
+    println!("{}", path);
 }
